f1fb98cebb6141f94f4d09203da238c4218168a0
Mahad Kalam <55807755+SkyfallWasTaken@users.noreply.github.com>
2025-09-25 08:22:55 +0100

    turn off stickerlode scraper

--- src/scraping/scrapeAll.ts
+++ src/scraping/scrapeAll.ts
@@ -4,7 +4,7 @@ import { StickerlodeScraper } from "./stickerlode";
 import { regions, ShopItems } from "./index";
 
 export async function scrapeAll(cookie: string): Promise<ShopItems> {
-  const scrapers = [new RegularScraper(cookie), new BlackMarketScraper(cookie), new StickerlodeScraper(cookie)];
+  const scrapers = [new RegularScraper(cookie), new BlackMarketScraper(cookie)];
   const results = await Promise.all(scrapers.map(scraper => scraper.scrape()));
   const flat = results.flat();
   console.log(`🎉 Found ${flat.length} items with ${scrapers.length} scrapers and ${regions.length} regions.`);
